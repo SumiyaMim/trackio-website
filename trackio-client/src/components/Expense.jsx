fc7a27e9f94a0a169ac44f5c2172c7e1e0c89db7
SumiyaMim <sumiyaislam1093@gmail.com>
2025-01-03 20:25:55 +0600

    add expense section

--- trackio-client/src/components/Expense.jsx
+++ trackio-client/src/components/Expense.jsx
@@ -18,26 +18,41 @@ const expense = () => {
 
   return (
     <div className="max-w-7xl mx-auto py-20 px-5 md:px-10">
-      <div className="flex items-center gap-20">
-        <div className="w-1/2">
-          <h2 className="font-bold text-5xl mb-4 text-[#4a4a4a]">Manage Tracking</h2>
-          <h2 className="font-bold text-5xl text-[#FF6F61] mb-9">Predict Your Expenses</h2>
-          <p className="font-light text-lg italic mb-10">Easily track your expenses, see your spending habits, and predict future costs with our simple system.</p>
+      <div className="flex flex-col lg:flex-row items-center gap-20">
+        <div className="w-full lg:w-1/2">
+          <h2 className="font-bold text-4xl md:text-5xl mb-4 text-[#4a4a4a]">Manage Tracking</h2>
+          <h2 className="font-bold text-4xl md:text-5xl text-[#FF6F61] mb-9">Predict Your Expenses</h2>
+          <p className="font-light text-lg italic mb-10">Easily track your expenses, see your spending habits, and predict future costs with our simple, efficient, and reliable system.</p>
           <div className="flex items-center gap-4">
             <button className="bg-[#406EA2] text-white text-lg font-semibold px-6 py-3 rounded">Get Started</button>
-            <button className="bg-[#e0eefc] text-[#406EA2] text-lg font-semibold px-6 py-3 rounded">Learn More</button>
+            <button className="bg-[#e8f4ff] text-[#406EA2] text-lg font-semibold px-6 py-3 rounded">Learn More</button>
           </div>
         </div>
         {/* Input Section */}
-        <div className="w-1/2">
-          <h4 className="text-center text-lg font-semibold mb-14">Expense Record</h4>
-          <div className="relative mb-10">
+        <div className="w-full lg:w-1/2 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] p-10 md:p-16 lg:p-14 rounded-md">
+          <h4 className="text-center text-xl font-semibold mb-14 text-[#406EA2]">Expense Record</h4>
+          <div className="relative z-10 mb-8">
+              <input
+              id="title"
+              type="text"
+              className="w-full peer block appearance-none border-0 border-b-2 border-b-[#cccccc] border-gray-700 bg-transparent px-0 py-3 text-sm text-gray-700 font-medium focus:border-[#406EA2] focus:outline-none focus:ring-0"
+              placeholder=" "
+              />
+              <label
+              htmlFor="title"
+              className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-base font-medium text-gray-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#406EA2] rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
+              >
+              Title
+              </label>
+          </div>
+          <div className="relative z-10 mb-8">
             <select
               id="category"
-              className="w-full border-b-2 border-b-[#cccccc] py-1.5 focus:outline-none focus:border-[#406EA2] focus:border-b-2 transition-colors peer"
-              autoComplete="off"
+              className="w-full peer block appearance-none border-0 border-b-2 border-b-[#cccccc] border-gray-700 bg-transparent px-0 py-3 text-sm text-gray-700 font-medium focus:border-[#406EA2] focus:outline-none focus:ring-0"
+              placeholder=" "
+              defaultValue=""
             >
-              <option value="" disabled selected hidden></option>
+              <option value="" disabled hidden>Select Category</option>
               <option value="Commute">Commute</option>
               <option value="Eating Out">Eating Out</option>
               <option value="Entertainment">Entertainment</option>
@@ -51,60 +66,45 @@ const expense = () => {
             </select>
             <label
               htmlFor="category"
-              className="absolute left-0 font-medium top-1 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-[#406EA2] transition-all"
+              className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-base font-medium text-gray-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#406EA2] rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
             >
               Category
             </label>
           </div>
-          <div className="relative mb-10">
+          <div className="relative z-10 mb-8">
             <input
+              id="incurred-on"
               type="text"
-              id="date"
               value={currentDateTime}
-              className="w-full border-b-2 border-b-[#cccccc] py-1.5 focus:outline-none focus:border-[#406EA2] focus:border-b-2 transition-colors peer"
-              autoComplete="off"
+              className="w-full peer block appearance-none border-0 border-b-2 border-b-[#cccccc] border-gray-700 bg-transparent px-0 py-3 text-sm text-gray-700 font-medium focus:border-[#406EA2] focus:outline-none focus:ring-0"
+              placeholder=" "
               readOnly
             />
             <label
-              htmlFor="date"
-              className="absolute left-0 font-medium text-gray-600 cursor-text top-[-8px] text-[#406EA2] text-xs transition-all"
+              htmlFor="incurred-on"
+              className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-base font-medium text-gray-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#406EA2] rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
             >
               Incurred on
             </label>
           </div>
-          
-
-        <div className="relative z-10 mb-8">
-            <input
-            id="title"
-            type="text"
-            className="w-full peer block appearance-none border-0 border-b-2 border-b-[#cccccc] border-gray-700 bg-transparent px-0 py-3 text-sm text-gray-700 font-medium focus:border-[#406EA2] focus:outline-none focus:ring-0"
-            placeholder=" "
-            />
-            <label
-            htmlFor="title"
-            className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-base font-medium text-gray-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#406EA2] rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
-            >
-            Title
-            </label>
-        </div>
-        <div className="relative z-10">
-            <input
-            id="amount"
-            type="number"
-            className="w-full peer block appearance-none border-0 border-b-2 border-b-[#cccccc] border-gray-700 bg-transparent px-0 py-3 text-sm text-gray-700 font-medium focus:border-[#406EA2] focus:outline-none focus:ring-0"
-            placeholder=" "
-            />
-            <label
-            htmlFor="amount"
-            className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-base font-medium text-gray-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#406EA2] rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
-            >
-              Amount &nbsp;<span className="text-xs">(TK.)</span>
-            </label>
-        </div>
-
-
-
+          <div className="relative z-10 mb-14">
+              <input
+              id="amount"
+              type="number"
+              className="w-full peer block appearance-none border-0 border-b-2 border-b-[#cccccc] border-gray-700 bg-transparent px-0 py-3 text-sm text-gray-700 font-medium focus:border-[#406EA2] focus:outline-none focus:ring-0"
+              placeholder=" "
+              />
+              <label
+              htmlFor="amount"
+              className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-base font-medium text-gray-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#406EA2] rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
+              >
+                Amount &nbsp;<span className="text-xs">(TK.)</span>
+              </label>
+          </div>
+          <div className="flex items-center justify-between">
+            <button className="bg-[#406EA2] text-white text-base font-semibold px-5 py-2 rounded">Submit</button>
+            <button className="bg-[#ececec] text-base font-semibold px-5 py-2 rounded">Cancel</button>
+          </div>
         </div>
       </div>
     </div>
